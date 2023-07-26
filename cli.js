29549d793e0e21ebf7da48daf10b20a2fe49d0df
Allan Gaiteiro <dev4@capitalempreendedor.com.br>
2023-07-26 08:54:22 -0300

    feat: change for index by cli

---
+++ cli.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+import { program } from "commander";
+import { ControllerLib } from "./lib/controller/controller.lib.js";
+import { RepositoryLib } from "./lib/repository/repository.lib.js";
+import { ServiceLib } from "./lib/service/service.lib.js";
+// Comandos da linha de comando
+program.version("1.0.0");
+
+program
+  .command("create-repository <class-name>")
+  .description("Cria um repositório para a classe especificada.")
+  .action((className) => {
+    RepositoryLib.createRepository(className);
+  });
+
+program
+  .command("create-service <class-name>")
+  .description("Cria um service para a classe especificada.")
+  .action((className) => {
+    ServiceLib.createService(className);
+  });
+
+program
+  .command("create-controller <class-name>")
+  .description("Cria um controller para a classe especificada.")
+  .action((className) => {
+    ControllerLib.createController(className);
+  });
+
+program
+  .command("validate-repository <class-name>")
+  .description(
+    "Valida o repositório e atualiza o serviço com os métodos encontrados."
+  )
+  .action((className) => {
+    const methods =
+      RepositoryLib.validateRepositoryAndGenerateMethods(className);
+    ServiceLib.updateServiceWithRepositoryMethods(className, methods);
+  });
+
+program.parse(process.argv);
+
+if (!program.args.length) {
+  program.help();
+}
